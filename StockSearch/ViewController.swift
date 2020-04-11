d0ede7f768877fe0d27953d31a44a15367fbbb3e
Tania Jasam <taniajasam@Tanias-MacBook-Air.local>
2020-04-11 14:51:32 +0530

    Project initialised

---
+++ StockSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StockSearch
+//
+//  Created by Tania Jasam on 11/04/20.
+//  Copyright © 2020 Tania Jasam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
